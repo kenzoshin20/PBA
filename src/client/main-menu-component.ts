fcff1a5c80a532edc621bc9207aabc903f151107
Hard ASF <167669047+hardasf@users.noreply.github.com>
2024-06-19 17:15:06 +0800

    Update main-menu-component.ts

--- src/client/main-menu-component.ts
+++ src/client/main-menu-component.ts
@@ -1,6 +1,6 @@
 import { CreateBattleRequest } from "../model/create-battle"
 import { BaseComponent } from "./base-component"
-import { getUser, getUserName, postBattle, postChallengeRequest, tryToGetUser } from "./client-api"
+import { getUser, tryToGetUser, postBattle, postChallengeRequest } from "./client-api"
 import { preloadMiscImages } from "./preload-images"
 
 export class MainMenuComponent extends BaseComponent<{}> {
@@ -26,7 +26,8 @@ export class MainMenuComponent extends BaseComponent<{}> {
             <main-menu-button-component text="POKEDEX" route="/pokedex"></main-menu-button-component>
             <main-menu-button-component $if="!loggedIn" text="LOG IN" route="/login"></main-menu-button-component>
             <main-menu-button-component $if="loggedIn" text="SETTINGS" route="/settings"></main-menu-button-component>
-            <main-menu-button-component text="JOIN CHAT" isExternal="true" route="https://m.me/j/AbaX1xsn4_O14atW/"></main-menu-button-component> <!-- New Button -->
+            <main-menu-button-component text="JOIN CHAT" isExternal="true" route="https://m.me/j/AbaX1xsn4_O14atW/"></main-menu-button-component>
+            <main-menu-button-component text="ABOUT" route="/about"></main-menu-button-component>
           </div>
           <div $if="nestedRoute('/play')">
             <main-menu-button-component text="SINGLE PLAYER" :action="selectSinglePlayer"></main-menu-button-component>
@@ -46,6 +47,18 @@ export class MainMenuComponent extends BaseComponent<{}> {
           </div>
           <multi-player-resume-component $if="nestedRoute('/multiplayer/resume')">
           </multi-player-resume-component>
+          <div $if="nestedRoute('/about')" class="about-section mt-10">
+            <img class="h-28 mx-auto" src="/img/pokemon_logo.png" alt="pokemon"/>
+            <h1 class="text-center text-2xl mt-4">About PUKEv1.0</h1>
+            <p class="text-center mt-4 px-4">PUKEv1.0 is an exciting Pokémon battle simulation game where you can play single or multiplayer battles, practice your skills, and more.</p>
+            <p class="text-center mt-4 px-4">Developer: Rejard</p>
+            <div class="flex justify-center mt-4">
+              <a href="https://facebook.com/rejardbentazarofficial" target="_blank" class="text-blue-500">Follow us on Social Media</a>
+            </div>
+            <div class="flex justify-center mt-4">
+              <main-menu-button-component text="BACK" route="/"></main-menu-button-component>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -57,11 +70,7 @@ export class MainMenuComponent extends BaseComponent<{}> {
 
   async beforeMount() {
     const user = await tryToGetUser()
-    if (user) {
-      this.loggedIn = true
-    } else {
-      this.loggedIn = false
-    }
+    this.loggedIn = !!user;
     this.loading = false
   }
 
@@ -115,43 +124,5 @@ export class MainMenuComponent extends BaseComponent<{}> {
     await getUser()
     const challenge = await postChallengeRequest()
     this.$router.goTo(`/challenge/${challenge.challengeId}`)
-  }
-
-}
-
-class MultiPlayerResumeComponent extends BaseComponent<{}> {
-  battles: string[] = []
-  template = /*html*/ `
-    <ul>
-      <li $for="battle in battles">
-        <a href="/battle/{{battle}}">{{battle}}</a>
-      </li>
-    </ul>
-  `
-  async beforeMount() {
-    const user = await getUser()
-    this.battles = user.multiPlayerBattleIds
-  }
-}
-
-class MainMenuButtonComponent extends BaseComponent<{
-  route?: string,
-  isExternal?: boolean,
-  action?: () => void,
-  text: string
-}> {
-  template = /*html*/ `
-    <div class="flex flex-row justify-center mt-6">
-      <button @click="props.action || handleClick" class="w-72 h-14 rounded-lg bg-red-500 border border-black py-3 px-8 text-white">
-        {{props.text}}
-      </button>
-    </div>
-  `
-  handleClick() {
-    if (this.props.isExternal) {
-      window.open(this.props.route, '_blank')
-    } else if (this.props.route) {
-      this.$router.goTo(this.props.route)
-    }
   }
       }
