5e03241cf41a558e4f29bdebc9582e0fd684721d
Hard ASF <167669047+hardasf@users.noreply.github.com>
2024-06-27 08:41:03 +0800

    Add files via upload

--- src/client/challenge-component.ts
+++ src/client/challenge-component.ts
@@ -7,13 +7,13 @@ import { TerminalComponent } from "./terminal-component";
 
 export class ChallengeComponent extends BaseComponent<{
   routeParams: {
-    challengeId: string;
-  };
+    challengeId: string
+  }
 }> {
-  view: 'LOADING' | 'CHALLENGER' | 'RECEIVER' = 'LOADING';
-  challenge?: Challenge;
-  url?: string;
-  showCopiedMsg = false;
+  view: 'LOADING' | 'CHALLENGER' | 'RECEIVER' = 'LOADING'
+  challenge?: Challenge
+  url?: string
+  showCopiedMsg = false
   template = /*html*/ `
     <div class="py-3 font-mono container mx-auto px-4" style="max-width: 500px">
 
@@ -49,113 +49,96 @@ export class ChallengeComponent extends BaseComponent<{
           <div @click="shareChallenge" class="ml-1 mt-2 h-16 cursor-pointer bg-gray-100 text-center text-lg pt-3 rounded border-2 border-solid border-black">
             Share
           </div>
-          
           <div @click="rejectChallenge" class="mr-1 mt-2 h-16 cursor-pointer bg-gray-100 text-center text-lg pt-3 rounded border-2 border-solid border-black">
             Cancel
           </div>
-         
         </div>
         <p class="text-center mt-2">Challenger is waiting for rival...</p>
         <p $if="showCopiedMsg" class="text-center mt-2">Copied!</p>
       </div>
 
     </div>
-  `;
+  `
   includes = {
     TerminalComponent
-  };
-
+  }
   async beforeMount() {
-    const loggedInUser = await tryToGetUser();
-    const challengeId = this.props.routeParams.challengeId;
+    const loggedInUser = await tryToGetUser()
+    const challengeId = this.props.routeParams.challengeId
     if (challengeId) {
-      this.url = document.location.href;
-      this.challenge = await getChallenge(challengeId);
+      this.url = document.location.href
+      this.challenge = await getChallenge(challengeId)
       if (loggedInUser && this.challenge.challengerName === loggedInUser.username) {
-        this.view = 'CHALLENGER';
+        this.view = 'CHALLENGER'
         await this.$controller.publish({
           type: 'DISPLAY_MESSAGE',
           message: `Challenger is waiting for rival...`
-        });
-        this.poll();
+        })
+        this.poll()
       } else {
-        this.view = 'RECEIVER';
+        this.view = 'RECEIVER'
         await this.$controller.publish({
           type: 'DISPLAY_MESSAGE',
           message: `${this.challenge?.challengerName} challenged you to a Pokemon battle!`
-        });
+        })
       }
     } else {
-      this.$router.goTo('/');
+      this.$router.goTo('/')
     }
   }
 
   async poll() {
     if (this.challenge) {
-      const poller = new Poller();
+      const poller = new Poller()
       poller.action = async () => {
-        this.challenge = await getChallenge(this.challenge!.challengeId);
-      };
+        this.challenge = await getChallenge(this.challenge!.challengeId)
+      }
       poller.endCondition = async () => {
-        return this.challenge?.state === 'ACCEPTED';
-      };
-      await poller.run();
-      this.$router.goTo(`/battle/${this.challenge.battleId}`);
+        return this.challenge?.state === 'ACCEPTED'
+      }
+      await poller.run()
+      this.$router.goTo(`/battle/${this.challenge.battleId}`)
     }
   }
 
   async acceptChallenge() {
-    const challengeId = this.props.routeParams.challengeId;
+    const challengeId = this.props.routeParams.challengeId
     if (challengeId) {
-      const challenge = await postChallengeAccept(challengeId);
+      const challenge = await postChallengeAccept(challengeId)
       if (challenge.battleId) {
-        this.$router.goTo(`/battle/${challenge.battleId}`);
+        this.$router.goTo(`/battle/${challenge.battleId}`)
       } else {
-        alert('Failed to accept challenge');
+        alert('Failed to accept challenge')
         logInfo('Failed to accept challenge, no battleId');
         logInfo(challenge);
       }
     }
   }
 
-  async rejectChallenge() {
-    this.$router.goTo('/');
+  rejectChallenge() {
+    this.$router.goTo('/')
   }
 
   async shareChallenge() {
-    const challengeId = this.props.routeParams.challengeId;
-    if (challengeId) {
-      const challengerName = this.challenge?.challengerName;
-      const url = `http://158.101.198.227:8268/challenge?challenger=${challengerName}&challenge_id=${challengeId}`;
+    try {
+      // Copy URL to clipboard
+      const url = window.location.href;
+      await navigator.clipboard.writeText(url);
+      this.showCopiedMsg = true;
 
-      try {
-        const response = await fetch(url, {
-          method: 'POST'
-        });
+      // Send message to chat room
+      const loggedInUser = await tryToGetUser();
+      const challengeId = this.props.routeParams.challengeId;
 
-        if (response.ok) {
-          // Handle successful sharing of challenge
-          await this.$controller.publish({
-            type: 'DISPLAY_MESSAGE',
-            message: `Challenge shared successfully!`
-          });
-          this.showCopiedMsg = true;
-        } else {
-          // Handle error sharing challenge
-        /*  await this.$controller.publish({
-            type: 'DISPLAY_MESSAGE',
-            message: `Failed to share challenge`
-          }); */
-          console.error('Failed to share challenge:', response.statusText);
-        }
-      } catch (error) {
-        // Handle network or other errors
-    /*    console.error('Error sharing challenge:', error);
-        await this.$controller.publish({
-          type: 'DISPLAY_MESSAGE',
-          message: `Error sharing challenge: ${error.message}`
-        }); */
+      if (loggedInUser && challengeId) {
+        const message = `${loggedInUser.username} wants to challenge you: ${url}`;
+        await sendMessageToChatRoom('pba125', 'SYSTEM', message);
+        console.log('Message sent successfully');
+      } else {
+        console.error('Failed to send message: User not logged in or challengeId not available.');
       }
+    } catch (error) {
+      console.error('Error sharing challenge:', error.message);
     }
   }
 }
